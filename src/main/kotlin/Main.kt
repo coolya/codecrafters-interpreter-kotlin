be6f256787c846cbc66e060913a2b8e251e0899e
Kolja Dummann <kolja@hey.com>
2025-04-29 08:07:56 +0200

    [any message]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import kotlin.system.exitProcess
 
 
 enum class TokenType {
-    LEFT_PAREN, RIGHT_PAREN, EOF, LEFT_BRACE, RIGHT_BRACE
+    LEFT_PAREN, RIGHT_PAREN, EOF, LEFT_BRACE, RIGHT_BRACE, STAR, DOT, COMMA, PLUS, MINUS, SLASH, SEMICOLON
 }
 
 data class Token(val type: TokenType, val lexeme: String) {
@@ -32,17 +32,23 @@ fun main(args: Array<String>) {
     // Uncomment this block to pass the first stage
     val tokenSteam = mutableListOf<Token>()
     if (fileContents.isNotEmpty()) {
-         val chars = fileContents.toCharArray()
-         for (char in chars) {
-             when (char) {
-                 '(' -> tokenSteam.add(Token(TokenType.LEFT_PAREN, "("))
-                 ')' -> tokenSteam.add(Token(TokenType.RIGHT_PAREN, ")"))
-                 '{' -> tokenSteam.add(Token(TokenType.LEFT_BRACE, "{"))
-                 '}' -> tokenSteam.add(Token(TokenType.RIGHT_BRACE, "}"))
-                 else -> println("UNKNOWN_CHAR")
-             }
-         }
-     }
+        val chars = fileContents.toCharArray()
+        for (char in chars) {
+            when (char) {
+                '(' -> tokenSteam.add(Token(TokenType.LEFT_PAREN, "("))
+                ')' -> tokenSteam.add(Token(TokenType.RIGHT_PAREN, ")"))
+                '{' -> tokenSteam.add(Token(TokenType.LEFT_BRACE, "{"))
+                '}' -> tokenSteam.add(Token(TokenType.RIGHT_BRACE, "}"))
+                '*' -> tokenSteam.add(Token(TokenType.STAR, "*"))
+                ',' -> tokenSteam.add(Token(TokenType.COMMA, ","))
+                '.' -> tokenSteam.add(Token(TokenType.DOT, "."))
+                '-' -> tokenSteam.add(Token(TokenType.MINUS, "-"))
+                '+' -> tokenSteam.add(Token(TokenType.PLUS, "+"))
+                ';' -> tokenSteam.add(Token(TokenType.SEMICOLON, ";"))
+                else -> println("UNKNOWN_CHAR")
+            }
+        }
+    }
     tokenSteam.add(Token(TokenType.EOF, ""))
     println(tokenSteam.joinToString("\n"))
 }
