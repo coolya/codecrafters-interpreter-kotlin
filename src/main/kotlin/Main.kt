c8c84efd7879927d44d341d51a3782be123c86e2
Kolja Dummann <kolja@hey.com>
2025-04-29 07:53:21 +0200

    [any message]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 import java.io.File
 import kotlin.system.exitProcess
 
+
+enum class TokenType {
+    LEFT_PAREN, RIGHT_PAREN
+}
+
+data class Token(val type: TokenType, val lexeme: String) {
+    override fun toString(): String {
+        return "${type.name} $lexeme"
+    }
+}
+
 fun main(args: Array<String>) {
 
     if (args.size < 2) {
@@ -22,6 +33,16 @@ fun main(args: Array<String>) {
      if (fileContents.isNotEmpty()) {
          throw NotImplementedError("Scanner not implemented")
      } else {
+         val tokenSteam = mutableListOf<Token>()
+         val chars = fileContents.toCharArray()
+         for (char in chars) {
+             when (char) {
+                 '(' -> tokenSteam.add(Token(TokenType.LEFT_PAREN, "("))
+                 ')' -> tokenSteam.add(Token(TokenType.RIGHT_PAREN, ")"))
+                 else -> println("UNKNOWN_CHAR")
+             }
+         }
+         println(tokenSteam.joinToString(" numm \n"))
          println("EOF  null") // Placeholder, remove this line when implementing the scanner
      }
 }
